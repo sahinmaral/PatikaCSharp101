d113dc95f4b469543cf85b3764648154bc580c34
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-18 01:13:10 +0300

    added algorithmsSimpleArraySum

---
+++ AlgorithmsSimpleArraySum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsSimpleArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+            int result = SimpleArraySum(ar);
+        }
+        
+        public static int SimpleArraySum(List<int> ar)
+        {
+            int sum = 0;
+        
+            foreach(int number in ar){
+                sum += number;
+            }
+        
+            return sum;
+        }  
+    }
+}
