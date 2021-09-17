4e338ee0ed8c59b19f73712f42a94b6426d1c20c
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-17 10:26:43 +0300

    Added hackerRankDay2

---
+++ HackerRankDay2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HackerRankDay2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meal_cost = Convert.ToDouble(Console.ReadLine().Trim());
+
+            int tip_percent = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int tax_percent = Convert.ToInt32(Console.ReadLine().Trim());
+
+            solve(meal_cost,tip_percent,tax_percent);
+        }
+        
+        public static void solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+            double tip = (meal_cost * tip_percent) / 100 ;
+            double tax = (meal_cost * tax_percent) / 100;
+            
+            double totalCost = meal_cost + tip + tax;
+            
+            Console.WriteLine(Math.Round(totalCost));
+        }
+
+        
+    }
+}
