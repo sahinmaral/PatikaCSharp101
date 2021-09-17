d4f75a041f79b9da8462e9cc846073bcb9c8597b
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-17 12:35:27 +0300

    added hackerRankDay3

---
+++ HackerRankDay3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HackerRankDay3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+        
+            Console.WriteLine(CheckNumber(N));
+        }
+    
+        public static string CheckNumber(int number)
+        {
+            if (number % 2 == 1)
+            {
+                return "Weird";
+            }
+            else
+            {
+                if (number <= 2 && number >= 5)
+                {
+                    return "Not Weird";
+                }
+                else if (number >= 6 && number <= 20)
+                {
+                    return "Weird";
+                }
+                else
+                {
+                    return "Not Weird";
+                }
+            }
+        }
+    }
+}
