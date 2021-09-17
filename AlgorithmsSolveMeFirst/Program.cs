20c77d103b0eaa5e3fcf891c3e673b1a9047649c
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-17 11:00:00 +0300

    Added algorithmsSolveMeFirst

---
+++ AlgorithmsSolveMeFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AlgorithmsSolveMeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = solveMeFirst(val1,val2);
+            Console.WriteLine(sum);
+        }
+        
+        static int solveMeFirst(int a, int b) { 
+            return a+b;
+        }
+
+    }
+}
