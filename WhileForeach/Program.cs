eda618c7ff6debdef2e997365b3f1cb58ced80b2
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-17 23:50:22 +0300

    added whileForeach

---
+++ WhileForeach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WhileForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int counter = 1;
+            int sum = 0;
+            while (counter <= number)
+            {
+                sum += counter;
+                counter++;
+            }
+
+            Console.WriteLine(sum/number);
+
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+
+            string[] cars = { "BMW", "Ford", "Toyota", "Lexus" };
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+        }
+    }
+}
