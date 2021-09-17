7816a3b6264ab8103c93b23b5febda2e95bc54be
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-17 17:45:41 +0300

    added hackerRankDay1 , hackerRankDay4 , hackerRankDay5

---
+++ HackerRankDay1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HackerRankDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+            int i1 = int.Parse(Console.ReadLine());
+            double d1 = Convert.ToDouble(Console.ReadLine());
+            string s1 = Console.ReadLine();
+
+            int result1 = i + i1;
+            double result2 = d + d1;
+            string result3 = s + s1;
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2.ToString("F1"));
+            Console.WriteLine(result3);
+        }
+    }
+}
---
+++ HackerRankDay5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HackerRankDay5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            writeTableOfSum(n);
+        }
+
+        static void writeTableOfSum(int number)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(number + " x " + i + " = " + number * i);
+            }
+        }
+    }
+}
