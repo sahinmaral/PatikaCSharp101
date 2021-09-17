9a00d3e5a569ad465695ad4601e2cc83c84ddb94
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-17 12:05:45 +0300

    added ifElseIfTernaryIf

---
+++ IfElseIfTernaryIf/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IfElseIfTernaryIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time<12)
+            {
+                Console.WriteLine("Good morning");
+            }
+            else if(time >= 12 && time <= 18)
+            {
+                Console.WriteLine("Good evening");
+            }
+            else
+            {
+                Console.WriteLine("Good night");
+            }
+
+            string result = time < 12 ? "Good morning" : time < 18 ? "Good evening" : "Good night";
+
+            Console.WriteLine(result);
+        }
+    }
+}
