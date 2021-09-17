a7b205974f17c133ad58b846d3df1dd3a914460a
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-17 22:16:02 +0300

    added algorithmsStaircase , algorithmsMiniMaxSum

---
+++ AlgorithmsMiniMaxSum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsMiniMaxSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Int64> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp))
+                .ToList();
+
+            miniMaxSum(arr);
+        }
+
+        public static void miniMaxSum(List<Int64> arr)
+        {
+            List<Int64> sumOfNumbers = new List<Int64>();
+
+            Int64 sum = 0;
+
+            Int64 temp = 0;
+
+            for (int j = 0; j < arr.Count; j++)
+            {
+                for (int i = 0; i < arr.Count; i++)
+                {
+                    if (temp != i)
+                    {
+                        sum += arr[i];
+                    }
+                }
+                
+                sumOfNumbers.Add(sum);
+                temp++;
+                sum = 0;
+            }
+
+            Console.Write(sumOfNumbers.Min() + " " + sumOfNumbers.Max());
+            
+        }
+    }
+}
---
+++ AlgorithmsStaircase/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AlgorithmsStaircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            
+            MakeStaircase(length);
+        }
+
+        static void MakeStaircase(int number)
+        {
+            int temp = number;
+            
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 1; j < temp; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = number; j >= temp; j--)
+                {
+                    Console.Write("#");
+                }
+                
+                temp--;
+                Console.WriteLine("");
+            }
+        }
+    }
+}
