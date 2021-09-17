4f87987f7c7604522ef2a0f5992335055873c2d0
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-17 08:53:08 +0300

    First commit

---
+++ ConsoleProgramming/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Your name : ");
+            string name = Console.ReadLine();
+            
+            Console.Write("Your surname : ");
+            string surname = Console.ReadLine();
+            
+            Console.WriteLine("Your name , surname : "+name+surname);
+        }
+    }
+}
---
+++ HackerRankDay0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HackerRankDay0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string inputString = Console.ReadLine();
+            
+            Console.WriteLine(inputString);
+            
+            Console.ReadKey();
+        }
+    }
+}
