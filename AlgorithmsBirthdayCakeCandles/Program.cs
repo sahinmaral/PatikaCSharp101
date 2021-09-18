2f568c412b921d8af651577b8a40ea3e190f50b7
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-18 10:13:59 +0300

    added algorithmsBirthdayCakeCandles , algorithmsCompareTheTriplets

---
+++ AlgorithmsBirthdayCakeCandles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsBirthdayCakeCandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+            BirthdayCakeCandles(candles);
+        }
+        
+        public static int BirthdayCakeCandles(List<int> candles)
+        {
+            int max = 0;
+            int sum = 0;
+            
+            foreach (var number in candles)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }   
+            }
+
+            foreach (var number in candles)
+            {
+                if (number == max)
+                {
+                    sum++;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ AlgorithmsCompareTheTriplets/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsCompareTheTriplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+            Console.WriteLine(String.Join(" ", CompareTriplets(a, b)));
+
+        }
+        
+        public static List<int> CompareTriplets(List<int> a, List<int> b)
+        {
+            int winForA = 0;
+            int winForB = 0;
+            
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] > b[i]) winForA++;
+                else if (a[i] < b[i]) winForB++;
+            }
+
+            List<int> wins = new List<int>();
+            wins.Add(winForA);
+            wins.Add(winForB);
+
+            return wins;
+        }
+    }
+}
