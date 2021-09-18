82bf00a3b7a5b7a8e16a4d290b8e20987cd4dfe3
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-18 10:19:37 +0300

    added algorithmsAVeryBigSum

---
+++ AlgorithmsAVeryBigSum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsAVeryBigSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+            
+            Console.WriteLine(AVeryBigSum(ar));
+        }
+        
+        public static long AVeryBigSum(List<long> ar)
+        {
+            long sum = 0;
+            foreach(var number in ar){
+                sum += number;
+            }
+        
+            return sum;
+        }
+    }
+}
