a3bd81e86d4bdfaf7def7cc867af84b50c6847cb
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-19 01:34:40 +0300

    added enums

---
+++ Enums/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heat = 14;
+
+            if (heat <= (int)Weather.Normal)
+                Console.WriteLine("It is cold");
+            else if(heat >= (int)Weather.Hot)
+                Console.WriteLine("It is hot");
+            else if(heat >= (int)Weather.Normal && heat <= (int)Weather.Hot)
+                Console.WriteLine("Let's go outside");
+        }
+        
+
+        enum Weather
+        {
+            VeryCould = 0,
+            Cold = 5,
+            Normal = 15,
+            Hot = 20,
+            VeryHot = 25
+        }
+    }
+}
