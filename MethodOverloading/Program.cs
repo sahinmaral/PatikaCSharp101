9c7ac7a39bbfe259e44ee04c75e59dffbefe8c04
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-18 11:54:23 +0300

    added methodOverloading

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "999";
+            
+            bool status = int.TryParse(number, out int outNumber);
+            if (status)
+            {
+                Console.WriteLine("Success");
+                Console.WriteLine(outNumber);
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+
+            int temp;
+            
+            Methods methods = new Methods();
+            methods.Sum(2,3, out temp);
+            
+            methods.Sum(2,3);
+
+            Console.WriteLine(temp);
+        }
+    }
+
+    class Methods
+    {
+        public void Sum(int a, int b, out int sum)
+        {
+            sum = a + b;
+        }
+
+        public void Sum(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+    }
+}
