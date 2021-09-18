5daa2eec5efd942cbca580a92c7a7cfa34f1158d
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-18 09:25:04 +0300

    added methodsOfArray

---
+++ MethodsOfArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MethodsOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 23, 12, 4, 86, 72, 3, 11, 17 };
+
+            foreach (var number in arr)
+            {
+                Console.WriteLine(number);
+            }
+            
+            Array.Sort(arr);
+            
+            foreach (var number in arr)
+            {
+                Console.WriteLine(number);
+            }
+            
+            Array.Clear(arr,2,2);
+            
+            foreach (var number in arr)
+            {
+                Console.WriteLine(number);
+            }
+            
+            Array.Reverse(arr);
+            
+            foreach (var number in arr)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine(Array.IndexOf(arr, 23));
+            
+            
+            
+        }
+    }
+}
