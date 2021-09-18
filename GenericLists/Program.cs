024af74437277c9daaf4d3e720d64ab9b58817d0
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-18 13:06:11 +0300

    added genericLists , hackerRankDay21

---
+++ GenericLists/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+
+            List<string> colours = new List<string>();
+            colours.Add("brown");
+            colours.Add("green");
+            colours.Add("yellow");
+            colours.Add("red");
+
+            Console.WriteLine($"colours.Count : {colours.Count}");
+            
+            colours.ForEach(x=>Console.WriteLine(x));
+
+            colours.Remove("brown");
+            numbers.RemoveAt(0);
+            
+            if(colours.Contains("brown")) Console.WriteLine("Brown found");
+            else Console.WriteLine("Brown cannot find");
+            
+            colours.Clear();
+
+            List<User> users = new List<User>();
+            users.Add(new User()
+            {
+                Id = 1,
+                FullName = "Şahin MARAL"
+            });
+            users.Add(new User()
+            {
+                Id = 2,
+                FullName = "Ali MARAL"
+            });
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.FullName);
+            }
+        }
+    }
+
+    class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ HackerRankDay21/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HackerRankDay21
+{
+    class Printer
+    {
+        static void PrintArray<T>(T[] variable){
+            foreach (var item in variable)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+		
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] stringArray = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                stringArray[i] = Console.ReadLine();
+            }
+		
+            PrintArray<Int32>(intArray);
+            PrintArray<String>(stringArray);
+        }
+    }
+}
