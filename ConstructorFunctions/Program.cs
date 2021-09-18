dace76061727b7c4d6330c9027464aa86ec30248
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-19 00:27:17 +0300

    added constructorFunctions

---
+++ ConstructorFunctions/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConstructorFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Şahin", "maral", 10, "Computer Engineering");
+            employee1.GetInfos();
+        }
+    }
+    
+    class Employee
+    {
+        public string Name { get; set; }
+
+        private string _surname;
+
+        public Employee(string name,string surname,int number,string department)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Department = department;
+            this.Number = number;
+        }
+        
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value.ToUpper(); }
+        }
+
+        public int Number { get; set; }
+        public string Department { get; set; }
+
+        public void GetInfos()
+        {
+            Console.WriteLine($"Name : {Name}");
+            Console.WriteLine($"Surname : {Surname}");
+            Console.WriteLine($"Number : {Number}");
+            Console.WriteLine($"Department : {Department}");
+        }
+    }
+}
