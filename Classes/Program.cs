d78ee3f486121ce7ce2b3eb39e1fe061b91d7952
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-19 00:22:19 +0300

    added classes

---
+++ Classes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee()
+            {
+                Name = "Şahin", Surname = "MARAL", Number = 5, Department = "Computer Engineering"
+            };
+
+            employee1.GetInfos();
+        }
+    }
+
+    class Employee
+    {
+        public string Name { get; set; }
+
+        private string _surname;
+
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value.ToUpper(); }
+        }
+
+        public int Number { get; set; }
+        public string Department { get; set; }
+
+        public void GetInfos()
+        {
+            Console.WriteLine($"Name : {Name}");
+            Console.WriteLine($"Surname : {Surname}");
+            Console.WriteLine($"Number : {Number}");
+            Console.WriteLine($"Department : {Department}");
+        }
+    }
+}
