8a90f6f5d10cc88f36e35cafc72860dd71be8ffd
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-09-19 15:34:53 +0300

    added hackerRankDay16

---
+++ HackerRankDay16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HackerRankDay16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            try
+            {
+                var i = int.Parse(S);
+                Console.Write(i);
+            }
+            catch
+            {
+                Console.WriteLine("Bad String");
+            }
+
+        }
+    }
+}
